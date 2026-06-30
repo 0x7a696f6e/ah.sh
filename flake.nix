1bcda69b1f8e38fb7de48f52648eaeda165e8b96
zion <ox7a696f6e@outlook.com>
2026-06-30 20:34:29 +0800

    build(nix): migrate to fenix + crane
    - Replace rust-overlay with fenix (toolchain) + crane (build)
    - Add cargoClippy, cargoAudit, cargoNextest checks

--- flake.nix
+++ flake.nix
@@ -1,38 +1,45 @@
 {
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/0.1";
-    systems = {
-      url = "github:nix-systems/default";
-      flake = false;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     git-hooks-nix = {
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    systems = {
+      url = "github:nix-systems/default";
+      flake = false;
+    };
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
     };
   };
 
   outputs =
-    inputs@{ self, ... }:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    { self, ... }@inputs:
+
+    with inputs;
+
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import systems;
 
       imports = [
-        inputs.treefmt-nix.flakeModule
-        inputs.git-hooks-nix.flakeModule
+        treefmt-nix.flakeModule
+        git-hooks-nix.flakeModule
       ];
-
-      systems = import inputs.systems;
 
       perSystem =
         {
@@ -42,42 +49,55 @@
           lib,
           ...
         }:
+        let
+          fenixPkgs = fenix.packages.${system};
+          craneLib = (crane.mkLib pkgs).overrideToolchain fenixPkgs.latest.toolchain;
+          src = lib.cleanSourceWith {
+            filter =
+              path: type: (builtins.match ".*assets/.*" path) != null || craneLib.filterCargoSources path type;
+            src = ./.;
+          };
+          commonArgs = {
+            inherit src;
+            strictDeps = true;
+          };
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          ah = craneLib.buildPackage (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              doCheck = false;
+            }
+          );
+        in
         {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [ inputs.rust-overlay.overlays.default ];
+          checks = {
+            inherit ah;
+            cargoClippy = craneLib.cargoClippy (
+              commonArgs
+              // {
+                inherit cargoArtifacts;
+                cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+              }
+            );
+            cargoAudit = craneLib.cargoAudit { inherit src advisory-db; };
+            cargoNextest = craneLib.cargoNextest (commonArgs // { inherit cargoArtifacts; });
           };
 
-          packages =
-            let
-              cargoToml = fromTOML (builtins.readFile (self + /Cargo.toml));
-            in
-            {
-              ah = pkgs.rustPlatform.buildRustPackage {
-                inherit (cargoToml.package) version;
-                pname = cargoToml.package.name;
-                src = ./.;
-                cargoLock.lockFile = ./Cargo.lock;
-              };
-              default = self.packages.${system}.ah;
-            };
-
+          packages = {
+            inherit ah;
+            default = ah;
+          };
           apps.default = {
             type = "app";
-            program = "${self.packages.${system}.default}/bin/ah";
+            program = lib.getExe' ah "ah";
           };
 
-          devShells.default = pkgs.mkShellNoCC {
+          devShells.default = craneLib.devShell {
             inherit (config.pre-commit.devShell) shellHook;
-
-            packages = [
-              pkgs.nixd
-              pkgs.cargo-edit
-              pkgs.cargo-audit
-              pkgs.rust-bin.stable.latest.complete
-            ]
-            ++ (lib.attrValues config.treefmt.build.programs)
-            ++ config.pre-commit.settings.enabledPackages;
+            checks = self.checks.${system};
+            packages =
+              (lib.attrValues config.treefmt.build.programs) ++ config.pre-commit.settings.enabledPackages;
           };
 
           pre-commit.settings.hooks = {
@@ -103,20 +123,16 @@
                 enable = true;
               });
 
-          treefmt = {
-            programs = {
-              nixfmt.enable = true;
-              prettier.enable = true;
-              taplo.enable = true;
-              rustfmt = {
-                enable = true;
-                package = pkgs.rust-bin.stable.latest.rustfmt;
-              };
+          treefmt.programs = {
+            nixfmt.enable = true;
+            prettier.enable = true;
+            taplo.enable = true;
+            rustfmt = {
+              enable = true;
+              package = fenixPkgs.latest.rustfmt;
             };
-            settings.excludes = [
-              "src/assets/config.schema.json"
-            ];
           };
+          treefmt.settings.excludes = [ "src/assets/config.schema.json" ];
         };
     };
 }
